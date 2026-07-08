d7688188d123a9413a5b1881d9f5c62e0057454c
Joschka Thurner <joschka.thurner@retoflow.de>
2026-07-08 09:37:05 +0200

    set version to 0.14.0-retoflow.1

--- src/pandapipes/__init__.py
+++ src/pandapipes/__init__.py
@@ -17,7 +17,7 @@ except importlib.metadata.PackageNotFoundError:
             if "project" in data and "version" in data["project"]:
                 __version__ = data["project"]["version"]
 
-__format_version__ = '0.14.0'
+__format_version__ = '0.14.0.retoflow.1'
 
 import pandas as pd
 import os
